f1bacaabf5f9b492fcae8a4848d5a3d59bf99311
Piterowsky <taatarski@gmail.com>
2020-02-08 12:30:33 +0100

    Add main menu

---
+++ app/src/main/java/pl/piterowsky/runinga/MainMenu.kt
@@ -0,0 +1,16 @@
+package pl.piterowsky.runinga
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_main_menu.*
+
+class MainMenu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+
+}
